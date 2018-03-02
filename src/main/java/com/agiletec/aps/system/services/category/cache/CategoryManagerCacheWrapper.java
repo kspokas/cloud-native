ddf2d9ae1f3332bc52c41bb1170d61ec3d3a4d63
Eugenio Santoboni <eugenio.santoboni@gmail.com>
2018-03-02 10:43:22 +0100

    change cache container

--- src/main/java/com/agiletec/aps/system/services/category/cache/CategoryManagerCacheWrapper.java
+++ src/main/java/com/agiletec/aps/system/services/category/cache/CategoryManagerCacheWrapper.java
@@ -33,129 +33,129 @@ import org.slf4j.LoggerFactory;
  */
 public class CategoryManagerCacheWrapper extends AbstractCacheWrapper implements ICategoryManagerCacheWrapper {
 
-	private static final Logger logger = LoggerFactory.getLogger(CategoryManagerCacheWrapper.class);
+    private static final Logger logger = LoggerFactory.getLogger(CategoryManagerCacheWrapper.class);
 
-	@Override
-	public void initCache(ICategoryDAO categoryDAO, ILangManager langManager) throws ApsSystemException {
-		System.out.println("INIZIALIZZAZIONE CACHE CATEGORY -> " + this);
-		List<Category> categories = null;
-		try {
-			this.releaseCachedObjects();
-			categories = categoryDAO.loadCategories(langManager);
-			if (categories.isEmpty()) {
-				Category root = this.createRoot(langManager);
-				categoryDAO.addCategory(root);
-				categories.add(root);
-			}
-			this.initCache(categories);
-		} catch (Throwable t) {
-			logger.error("Error loading the category tree", t);
-			throw new ApsSystemException("Error loading the category tree.", t);
-		}
-		System.out.println("CONCLUSA INIZIALIZZAZIONE CACHE CATEGORY -> " + this);
-	}
+    @Override
+    public void initCache(ICategoryDAO categoryDAO, ILangManager langManager) throws ApsSystemException {
+        System.out.println("INIZIALIZZAZIONE CACHE CATEGORY -> " + this);
+        List<Category> categories = null;
+        try {
+            this.releaseCachedObjects();
+            categories = categoryDAO.loadCategories(langManager);
+            if (categories.isEmpty()) {
+                Category root = this.createRoot(langManager);
+                categoryDAO.addCategory(root);
+                categories.add(root);
+            }
+            this.initCache(categories);
+        } catch (Throwable t) {
+            logger.error("Error loading the category tree", t);
+            throw new ApsSystemException("Error loading the category tree.", t);
+        }
+        System.out.println("CONCLUSA INIZIALIZZAZIONE CACHE CATEGORY -> " + this);
+    }
 
-	protected Category createRoot(ILangManager langManager) {
-		Category root = new Category();
-		root.setCode("home");
-		root.setParentCode("home");
-		List<Lang> langs = langManager.getLangs();
-		ApsProperties titles = new ApsProperties();
-		for (Lang lang : langs) {
-			titles.setProperty(lang.getCode(), "Home");
-		}
-		root.setTitles(titles);
-		return root;
-	}
+    protected Category createRoot(ILangManager langManager) {
+        Category root = new Category();
+        root.setCode("home");
+        root.setParentCode("home");
+        List<Lang> langs = langManager.getLangs();
+        ApsProperties titles = new ApsProperties();
+        for (Lang lang : langs) {
+            titles.setProperty(lang.getCode(), "Home");
+        }
+        root.setTitles(titles);
+        return root;
+    }
 
-	private void initCache(List<Category> categories) throws ApsSystemException {
-		Category root = null;
-		Map<String, Category> categoryMap = new HashMap<>();
-		for (Category cat : categories) {
-			categoryMap.put(cat.getCode(), cat);
-			if (cat.getCode().equals(cat.getParentCode())) {
-				root = cat;
-			}
-		}
-		for (Category cat : categories) {
-			Category parent = categoryMap.get(cat.getParentCode());
-			if (cat != root) {
-				parent.addChildCode(cat.getCode());
-			}
-			cat.setParent(parent);
-		}
-		if (root == null) {
-			throw new ApsSystemException("Error found in the category tree: undefined root");
-		}
-		this.insertObjectsOnCache(root, categoryMap);
-	}
+    private void initCache(List<Category> categories) throws ApsSystemException {
+        Category root = null;
+        Map<String, Category> categoryMap = new HashMap<>();
+        for (Category cat : categories) {
+            categoryMap.put(cat.getCode(), cat);
+            if (cat.getCode().equals(cat.getParentCode())) {
+                root = cat;
+            }
+        }
+        for (Category cat : categories) {
+            Category parent = categoryMap.get(cat.getParentCode());
+            if (cat != root) {
+                parent.addChildCode(cat.getCode());
+            }
+            cat.setParent(parent);
+        }
+        if (root == null) {
+            throw new ApsSystemException("Error found in the category tree: undefined root");
+        }
+        this.insertObjectsOnCache(root, categoryMap);
+    }
 
-	protected void releaseCachedObjects() {
-		List<String> codes = (List<String>) this.getCustomCache().get(CATEGORY_CODES_CACHE_NAME);
-		if (null != codes) {
-			for (String code : codes) {
-				this.getCustomCache().remove(CATEGORY_CACHE_NAME_PREFIX + code);
-			}
-			this.getCustomCache().remove(CATEGORY_CODES_CACHE_NAME);
-		}
-		this.getCustomCache().remove(CATEGORY_STATUS_CACHE_NAME);
-	}
+    protected void releaseCachedObjects() {
+        List<String> codes = (List<String>) this.getCustomCache().get(CATEGORY_CODES_CACHE_NAME);
+        if (null != codes) {
+            for (String code : codes) {
+                this.getCustomCache().remove(CATEGORY_CACHE_NAME_PREFIX + code);
+            }
+            this.getCustomCache().remove(CATEGORY_CODES_CACHE_NAME);
+        }
+        this.getCustomCache().remove(CATEGORY_STATUS_CACHE_NAME);
+    }
 
-	protected void insertObjectsOnCache(Category root, Map<String, Category> categoryMap) {
-		this.getCustomCache().put(CATEGORY_ROOT_CACHE_NAME, root);
-		Iterator<Category> iter = categoryMap.values().iterator();
-		while (iter.hasNext()) {
-			Category category = iter.next();
-			this.getCustomCache().put(CATEGORY_CACHE_NAME_PREFIX + category.getCode(), category);
-		}
-	}
+    protected void insertObjectsOnCache(Category root, Map<String, Category> categoryMap) {
+        this.getCustomCache().put(CATEGORY_ROOT_CACHE_NAME, root);
+        Iterator<Category> iter = categoryMap.values().iterator();
+        while (iter.hasNext()) {
+            Category category = iter.next();
+            this.getCustomCache().put(CATEGORY_CACHE_NAME_PREFIX + category.getCode(), category);
+        }
+    }
 
-	@Override
-	public Category getCategory(String code) {
-		System.out.println("CHIESTA CATEGORY -> " + code);
-		return (Category) this.getCustomCache().get(CATEGORY_CACHE_NAME_PREFIX + code);
-	}
+    @Override
+    public Category getCategory(String code) {
+        System.out.println("CHIESTA CATEGORY -> " + code);
+        return (Category) this.getCustomCache().get(CATEGORY_CACHE_NAME_PREFIX + code);
+    }
 
-	@Override
-	public void deleteCategory(String code) {
-		this.getCustomCache().remove(CATEGORY_CACHE_NAME_PREFIX + code);
-	}
+    @Override
+    public void deleteCategory(String code) {
+        this.getCustomCache().remove(CATEGORY_CACHE_NAME_PREFIX + code);
+    }
 
-	@Override
-	public Category getRoot() {
-		return (Category) this.getCustomCache().get(CATEGORY_ROOT_CACHE_NAME);
-	}
+    @Override
+    public Category getRoot() {
+        return (Category) this.getCustomCache().get(CATEGORY_ROOT_CACHE_NAME);
+    }
 
-	@Override
-	protected String getCacheName() {
-		return CATEGORY_MANAGER_CACHE_NAME;
-	}
+    @Override
+    protected String getCacheName() {
+        return CATEGORY_MANAGER_CACHE_NAME;
+    }
 
-	@Override
-	public Map<String, Integer> getMoveNodeStatus() {
-		return (Map<String, Integer>) this.getCustomCache().get(CATEGORY_STATUS_CACHE_NAME);
-	}
+    @Override
+    public Map<String, Integer> getMoveNodeStatus() {
+        return (Map<String, Integer>) this.getCustomCache().get(CATEGORY_STATUS_CACHE_NAME);
+    }
 
-	@Override
-	public void updateMoveNodeStatus(String beanName, Integer status) {
-		Map<String, Integer> statusMap = this.getMoveNodeStatus();
-		if (null == statusMap) {
-			statusMap = new HashMap<>();
-		}
-		statusMap.put(beanName, status);
-		this.getCustomCache().put(CATEGORY_STATUS_CACHE_NAME, statusMap);
-	}
+    @Override
+    public void updateMoveNodeStatus(String beanName, Integer status) {
+        Map<String, Integer> statusMap = this.getMoveNodeStatus();
+        if (null == statusMap) {
+            statusMap = new HashMap<>();
+        }
+        statusMap.put(beanName, status);
+        this.getCustomCache().put(CATEGORY_STATUS_CACHE_NAME, statusMap);
+    }
 
-	public Map<String, Object> getCustomCache() {
-		System.out.println("CACHE -> " + this.customCache.getClass());
-		return customCache;
-	}
+    public Map<String, Object> getCustomCache() {
+        System.out.println("CACHE -> " + this.customCache.getClass());
+        return customCache;
+    }
 
-	public void setCustomCache(Map<String, Object> customCache) {
-		this.customCache = customCache;
-	}
+    public void setCustomCache(Map<String, Object> customCache) {
+        this.customCache = customCache;
+    }
 
-	@Resource(lookup = "java:jboss/infinispan/server/Entando_CategoryManager")
-	private Map<String, Object> customCache;
+    @Resource(lookup = "java:jboss/infinispan/entando/Entando_CategoryManager")
+    private Map<String, Object> customCache;
 
 }
